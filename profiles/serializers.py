57a0876de6d640f59d7ddc2093566adce4cef7ab
Rachael Babister <rachaelbabister@gmail.com>
2024-07-24 19:18:27 +0000

    FEAT: Add Count and Filters fields to Profiles views and serializers

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -10,6 +10,9 @@ class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
     
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -31,5 +34,6 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = [
             'id', 'owner', 'created_on', 'updated_on', 'name', 
-            'description', 'image', 'is_owner', 'following_id'
+            'description', 'image', 'is_owner', 'following_id',
+            'posts_count', 'followers_count', 'following_count'
         ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,22 +1,41 @@
-from rest_framework import generics
+from django.db.models import Count
+from rest_framework import generics, filters
 from home_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(generics.ListCreateAPIView):
+class ProfileList(generics.ListAPIView):
     """
     List all profiles.
     No create view as profile creation is handled by django signals.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_on')
     serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter
+    ]
+    ordering_fields = [
+        'posts_count',
+        'followers_count',
+        'following_count',
+        'owner__following__created_on',
+        'owner__followed__created_on',
+    ]
     
     
 class ProfileDetail(generics.RetrieveUpdateAPIView):
     """
     Retrieve or update a profile if you are the owner.
     """
-    queryset = Profile.objects.all()
+    queryset = Profile.objects.annotate(
+        posts_count=Count('owner__post', distinct=True),
+        followers_count=Count('owner__followed', distinct=True),
+        following_count=Count('owner__following', distinct=True)
+    ).order_by('-created_on')
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
