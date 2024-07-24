c6fffa475fabb984000976bb1ea8fc43312e6250
Rachael Babister <rachaelbabister@gmail.com>
2024-07-24 13:31:35 +0000

    FEAT: Add Post serializer for the Post model, and input validation to check image size and dimensions

---
+++ posts/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Post model.
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    
+    def validate_image(self, value):
+        """
+        Validates the image size and dimensions.
+        Validation Error will appear if image size or dimensions exceed the limits.
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+    
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'title', 'content', 
+            'created_on', 'updated_on', 'image'
+        ]
