dcab2c7150fa4a24ecdf23b8e67d40d12dd55b82
Rachael Babister <rachaelbabister@gmail.com>
2024-07-24 20:48:14 +0000

    FEAT: Import Django filters to Comments App and add filter backends to CommentList in views

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
 from home_api.permissions import (IsOwnerOrReadOnly)
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -11,6 +12,9 @@ class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
