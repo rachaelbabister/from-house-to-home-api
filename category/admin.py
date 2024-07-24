178519af4e336a70291ccd95a1dd450c3076ae8f
Rachael Babister <rachaelbabister@gmail.com>
2024-07-24 21:47:24 +0000

    FIX: Register models in admin.py on all Apps

--- category/admin.py
+++ category/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
 from .models import Category
 
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    """
-    Admin view for Category.
-    """
-    list_display = ('id', 'name', 'created_on')
-    search_fields = ('name',)
-    list_per_page = (20)
+admin.site.register(Category)
