2cb9109b9625d49a7f8f9dae4d98ce9192d0edcc
Rachael Babister <rachaelbabister@gmail.com>
2024-07-24 18:14:39 +0000

    FEAT: Add FollowerList and FollowerDetail to views and add url paths

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from home_api.permissions import IsOwnerOrReadOnly
+from followers.models import Follower
+from followers.serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List or create follower relationships when logged in.
+    """
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follower.
+    'Destroy' a follower, i.e. unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
