ce3ceed15cf18de9221a1e069f0d044ee0a1a8c4
Rachael Babister <rachaelbabister@gmail.com>
2024-07-24 18:45:53 +0000

    FEAT: Add get likes and followers fields to Profiles and Posts serializers so that they can be seen when logged in

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,6 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
     
     def validate_image(self, value):
         """
@@ -33,11 +35,23 @@ class PostSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    def get_like_id(self, obj):
+        """
+        Retrieves the ID of the like linked to the post when logged in.
+        """
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         model = Post
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id',
-            'profile_image', 'title', 'content', 
-            'created_on', 'updated_on', 'image'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 
+            'title', 'content', 'created_on', 'updated_on', 'image',
+            'like_id'
         ]
