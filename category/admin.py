b2f733df368eaa01cd756cb7fa14f363f44b302b
Rachael Babister <rachaelbabister@gmail.com>
2024-07-28 13:59:56 +0000

    FIX: Amend created_on in admin.py

--- category/admin.py
+++ category/admin.py
@@ -5,11 +5,10 @@ from .models import Category
 class CategoryAdmin(admin.ModelAdmin):
     """
     Admin view for Category.
-
-    Displays 'id', 'name', and 'created_at'.
+    Displays 'id', 'name', and 'created_on'.
     Allows searching by 'name'.
     Shows 20 items per page.
     """
-    list_display = ('id', 'name', 'created_at')
+    list_display = ('id', 'name', 'created_on')
     search_fields = ('name',)
     list_per_page = (20)
