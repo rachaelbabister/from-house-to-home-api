5ea642a685de3c78b3df4bd26889a79262baaa80
Rachael Babister <rachaelbabister@gmail.com>
2024-07-28 13:49:21 +0000

    FIX: Add category to utls.py to fix url not working and update admin.py in category app

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
 from .models import Category
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin view for Category.
+
+    Displays 'id', 'name', and 'created_at'.
+    Allows searching by 'name'.
+    Shows 20 items per page.
+    """
+    list_display = ('id', 'name', 'created_at')
+    search_fields = ('name',)
+    list_per_page = (20)
