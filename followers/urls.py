0bf09c0b7be16cea94aa8757a3db2c563581d423
Rachael Babister <rachaelbabister@gmail.com>
2024-08-01 16:28:13 +0000

    FIX: Update url paths for likes and followers

--- followers/urls.py
+++ followers/urls.py
@@ -5,6 +5,6 @@ from followers import views
 urlpatterns = [
     path('followers/', views.FollowerList.as_view()),
     path('followers/<int:pk>/', views.FollowerDetail.as_view()),
-    path('followed-posts/', views.FollowedPostsList.as_view(),
+    path('followed-posts/', views.FollowedPosts.as_view(),
          name='followed-posts'),
 ]
--- likes/urls.py
+++ likes/urls.py
@@ -4,5 +4,5 @@ from likes import views
 urlpatterns = [
     path('likes/', views.LikeList.as_view()),
     path('likes/<int:pk>/', views.LikeDetail.as_view()),
-    path('liked-posts/', views.LikedPostsList.as_view(), name='liked-posts'),
+    path('liked-posts/', views.LikedPosts.as_view(), name='liked-posts'),
 ]
