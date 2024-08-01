3549fbb11636698eff5689083cc6b422beab7735
Rachael Babister <rachaelbabister@gmail.com>
2024-08-01 10:09:48 +0000

    FIX: Update Cors settings to try to fix change image in front end

--- home_api/settings.py
+++ home_api/settings.py
@@ -86,7 +86,7 @@ ALLOWED_HOSTS = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
+        os.environ.get('CLIENT_ORIGIN_DEV')
     ]
 else:
     CORS_ALLOWED_ORIGIN_REGEXES = [
